48f852608fb958492e500158657e72603ef02573
Montasir002 <mdmontasirsabit02@gmail.com>
2026-05-20 13:14:06 +0600

    Add files via upload

---
+++ commands/_more.js
@@ -0,0 +1,35 @@
+/*CMD
+  command: /more
+  help: 
+  need_reply: false
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+
+  ANSWER
+
+  <<KEYBOARD
+
+  KEYBOARD
+  aliases: 
+  group: 
+CMD*/
+
+/*CMD
+  command: /more
+  help: 
+  need_reply: false
+  auto_retry_time: 
+  folder: 
+  aliases: 
+  group: 
+CMD*/
+
+Bot.sendInlineKeyboard(
+  [
+    { title: "📩 Contact Admin", url: "https://t.me/Montasir00" },
+    { title: "🤖 More Bots",     url: "https://t.me/Ben_10_fan_bot" }
+  ],
+  "⚙️ More Options:"
+);
